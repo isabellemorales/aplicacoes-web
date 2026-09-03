95d2b75362508ade11a8c985d7dbc85d38b3024c
isabellemorales <isabellemoralesp@gmail.com>
2026-09-02 22:11:42 -0300

    criação do método post para envio de imagens

---
+++ src/main/java/com/example/imagemPecas/application/images/imageServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.imagemPecas.application.images;
+
+import com.example.imagemPecas.domain.entity.Image;
+import com.example.imagemPecas.domain.service.ImageService;
+import com.example.imagemPecas.infra.repository.imageRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class imageServiceImpl implements ImageService {
+
+    private final imageRepository repository;
+
+    @Override
+    @Transactional
+    public Image save(Image image) {
+        return  repository.save(image);
+    }
+}
